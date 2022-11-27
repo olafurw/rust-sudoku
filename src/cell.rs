6d79688d841116eaf0843eca479a2a559104ad4a
Olafur Waage <olafurw@gmail.com>
2022-11-27 04:05:51 +0100

    board and cell drawing.

--- src/cell.rs
+++ src/cell.rs
@@ -1,18 +1,28 @@
 use macroquad::prelude::*;
 
+#[derive(Clone)]
 pub struct Cell {
-    number: u32,
+    x: f32,
+    y: f32,
+    size: f32,
+    number: Option<u32>,
 }
 
 impl Cell {
     pub fn new() -> Self {
-        Self {
-            number: 9
+        Cell {
+            x: 0.0, y: 0.0, size: 0.0, number: None,
         }
     }
 
+    pub fn update(&mut self, x: f32, y: f32, size: f32) {
+        self.x = x;
+        self.y = y;
+        self.size = size;
+    }
+
     pub fn draw(&self) {
-        draw_rectangle(0.0, 0.0, 64.0, 64.0, WHITE);
-        draw_text("9", 100.0, 100.0, 100.0, BLACK);
+        draw_rectangle(self.x, self.y, self.size, self.size, WHITE);
+        draw_text("9", self.x, self.y, 100.0, BLACK);
     }
 }
