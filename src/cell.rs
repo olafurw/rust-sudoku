262bfcabf0ec1ff5e0121c136accf26188e78f65
Olafur Waage <olafurw@gmail.com>
2023-01-17 22:35:04 +0100

    adding cell tests

--- src/cell.rs
+++ src/cell.rs
@@ -19,6 +19,8 @@ impl Cell {
     }
 
     pub fn click(&mut self, x: f32, y: f32) -> bool {
+        self.selected = false;
+
         if x >= self.x && x <= self.x + self.size
         && y >= self.y && y <= self.y + self.size {
             self.selected = true;
@@ -31,5 +33,66 @@ impl Cell {
         self.x = x;
         self.y = y;
         self.size = size;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cell::Cell;
+
+    fn init_assert(cell: &Cell) {
+        assert_eq!(cell.x, 0.0);
+        assert_eq!(cell.y, 0.0);
+        assert_eq!(cell.size, 0.0);
+        assert_eq!(cell.number, Some(1));
+        assert_eq!(cell.selected, false);
+    }
+
+    fn click_assert(cell: &mut Cell, x: f32, y: f32, expected: bool) {
+        assert_eq!(cell.click(x, y), expected);
+        assert_eq!(cell.selected, expected);
+    }
+
+    #[test]
+    fn cell_init() {
+        let cell = Cell::new();
+        init_assert(&cell);
+    }
+
+    #[test]
+    fn cell_clear() {
+        let mut cell = Cell::new();
+        init_assert(&cell);
+
+        cell.clear();
+        assert_eq!(cell.selected, false);
+    }
+
+    #[test]
+    fn cell_click() {
+        let mut cell = Cell::new();
+        init_assert(&cell);
+
+        click_assert(&mut cell, 1.0, 1.0, false);
+        cell.update(0.0, 0.0, 32.0);
+        click_assert(&mut cell, 1.0, 1.0, true);
+        click_assert(&mut cell, 32.0, 32.0, true);
+        click_assert(&mut cell, 32.1, 32.1, false);
+
+        cell.update(0.0, 0.0, 64.0);
+        click_assert(&mut cell, 1.0, 1.0, true);
+        click_assert(&mut cell, 32.0, 32.0, true);
+        click_assert(&mut cell, 32.1, 32.1, true);
+        click_assert(&mut cell, 64.0, 64.0, true);
+        click_assert(&mut cell, 64.01, 64.01, false);
+
+        cell.update(32.0, 32.0, 32.0);
+        click_assert(&mut cell, 0.0, 0.0, false);
+        click_assert(&mut cell, 1.0, 1.0, false);
+        click_assert(&mut cell, 31.99, 31.99, false);
+        click_assert(&mut cell, 32.0, 32.0, true);
+        click_assert(&mut cell, 32.1, 32.1, true);
+        click_assert(&mut cell, 64.0, 64.0, true);
+        click_assert(&mut cell, 64.01, 64.01, false);
     }
 }
