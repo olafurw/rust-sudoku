1ee3b2f6afa0df57bad01ad5580f41d0c8bcac65
Olafur Waage <olafurw@gmail.com>
2023-04-03 21:06:53 +0200

    refactoring draw menu

--- src/draw_menu.rs
+++ src/draw_menu.rs
@@ -2,7 +2,30 @@ use macroquad::text::draw_text_ex;
 
 use crate::{board::BoardMode, context::Context, ICON_PENCIL, ICON_PENCIL_SLASH, ICON_UNDO};
 
-pub fn draw_menu(context: &Context) {
+fn draw_menu_pencil(context: &Context, icon_x_offset: f32, icon_y_offset: f32) {
+    let icon = match context.board.mode {
+        BoardMode::Pencil => ICON_PENCIL,
+        BoardMode::Normal => ICON_PENCIL_SLASH,
+    };
+
+    draw_text_ex(
+        icon,
+        context.menu.pencil.x + icon_x_offset,
+        context.menu.pencil.y + icon_y_offset + context.icon_font.height + (icon_x_offset / 2.0),
+        context.icon_font.params,
+    );
+}
+
+fn draw_menu_undo(context: &Context, icon_x_offset: f32, icon_y_offset: f32) {
+    draw_text_ex(
+        ICON_UNDO,
+        context.menu.undo.x + icon_x_offset,
+        context.menu.undo.y + icon_y_offset + context.icon_font.height + (icon_x_offset / 2.0),
+        context.icon_font.params,
+    );
+}
+
+fn draw_menu_numbers(context: &Context) {
     let font_x_offset = context.menu_number_font.width / 2.0;
     let font_y_offset = context.menu_number_font.height + (context.menu_number_font.height / 3.0);
 
@@ -15,26 +38,14 @@ pub fn draw_menu(context: &Context) {
             context.menu_number_font.params,
         );
     }
+}
+
+pub fn draw_menu(context: &Context) {
+    draw_menu_numbers(context);
 
     let icon_x_offset = (context.icon_font.width - context.menu.pencil.size).abs() / 2.0;
     let icon_y_offset = (context.icon_font.height - context.menu.pencil.size).abs() / 2.0;
 
-    let icon = match context.board.mode {
-        BoardMode::Pencil => ICON_PENCIL,
-        BoardMode::Normal => ICON_PENCIL_SLASH,
-    };
-
-    draw_text_ex(
-        icon,
-        context.menu.pencil.x + icon_x_offset,
-        context.menu.pencil.y + icon_y_offset + context.icon_font.height + (icon_x_offset / 2.0),
-        context.icon_font.params,
-    );
-
-    draw_text_ex(
-        ICON_UNDO,
-        context.menu.undo.x + icon_x_offset,
-        context.menu.undo.y + icon_y_offset + context.icon_font.height + (icon_x_offset / 2.0),
-        context.icon_font.params,
-    );
+    draw_menu_pencil(context, icon_x_offset, icon_y_offset);
+    draw_menu_undo(context, icon_x_offset, icon_y_offset);
 }
