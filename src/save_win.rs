3040f8923c0eb99de7688bc01c7f239788ee1b0f
Alex Rock Ancelet <pierstoval@gmail.com>
2024-05-21 12:36:38 +0200

    Migrate db to keep save

--- src/save_win.rs
+++ src/save_win.rs
@@ -1,7 +1,7 @@
-use rusqlite::{params, Connection};
+use rusqlite::{params, Connection, OpenFlags};
 
 pub fn load(key: &str) -> Option<String> {
-    let db = Connection::open("save.db").unwrap();
+    let db = get_connection();
     let mut stmt = db.prepare("SELECT value FROM save WHERE key = ?1").unwrap();
 
     let mut rows = stmt.query(params![key]).unwrap();
@@ -16,10 +16,37 @@ pub fn load(key: &str) -> Option<String> {
 }
 
 pub fn save(key: &str, value: &str) {
-    let db = Connection::open("save.db").unwrap();
+    let db = get_connection();
     db.execute(
         "REPLACE INTO save (key, value) VALUES (?1, ?2)",
         params![key, value],
     )
     .unwrap();
+}
+
+fn migrate_db(db: &Connection) {
+    let mut stmt = db.prepare("CREATE TABLE IF NOT EXISTS save (key VARCHAR(255) PRIMARY KEY, value LONGTEXT);").unwrap();
+    stmt.execute(params![]).unwrap();
+}
+
+fn get_connection() -> Connection {
+    let conn = Connection::open_with_flags("save.db", get_database_flags())
+        .expect("Could not open database.");
+
+    migrate_db(&conn);
+
+    return conn;
+}
+
+
+fn get_database_flags() -> OpenFlags {
+    let mut db_flags = OpenFlags::empty();
+
+    db_flags.insert(OpenFlags::SQLITE_OPEN_READ_WRITE);
+    db_flags.insert(OpenFlags::SQLITE_OPEN_CREATE);
+    db_flags.insert(OpenFlags::SQLITE_OPEN_FULL_MUTEX);
+    db_flags.insert(OpenFlags::SQLITE_OPEN_NOFOLLOW);
+    db_flags.insert(OpenFlags::SQLITE_OPEN_PRIVATE_CACHE);
+
+    db_flags
 }
